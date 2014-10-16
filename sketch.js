eb89d443574e5e3d40649df9ff7f9320fac31003
jmjvvv <jmjvvv@gmail.com>
2014-10-16 16:21:11 +0900

    d

--- sketch.js
+++ sketch.js
@@ -8,4 +8,19 @@ function draw()
 	background(0);
 	fill(255);
 	ellipse(mouseX, mouseY, 100, 100);
+
+	drawDonunt1(167,214,37,100,100);
+	drawDonunt1(63,214,37,103,300,100);
+	drawDonunt1(214,209,37255,400,200);
+	drawDonunt1(214,108,37,74,500,300);
+}
+
+function drawDonunt1(r,g,b,x, y)
+{
+
+	//do#1
+	fill(r,g,b,255);
+	ellipse(x,y,200,200);
+	fill(0);
+	ellipse(x,y, 120,120);
 }
